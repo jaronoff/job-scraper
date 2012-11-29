00593de0c385e44bf87623a25f64de7e156f94be
thejourneydude <journeyofthedude@gmail.com>
2012-11-28 23:18:38 -0500

    added css to home page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Clss::Application.configure do
       config.action_mailer.smtp_settings = {
         :address              => "smtp.gmail.com",
         :port                 => 587,
-        :user_name            => 'wouldntyouliketoknow',
-        :password             => 'wouldntyouliketoknow',
+        :user_name            => 'asdfasdf',
+        :password             => 'asdfasdf',
         :authentication       => 'plain',
         :enable_starttls_auto => true  
    }
